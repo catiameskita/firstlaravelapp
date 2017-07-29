e21d19ee59bf71c1d6055718c1e4cbbb4e786ea0
catiameskita <catiasofiamesquita@gmail.com>
2017-07-29 16:48:33 +0100

    Users with Photos pulling data to DB

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UsersRequest;
 
 use App\User;
 use App\Role;
+use App\Photo;
 
 class AdminUsersController extends Controller
 {
@@ -47,13 +48,33 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-        //
+        //First
+      /*  return $request->all();*/
 
-        User::create($request->all());
+        //Second
+       /* User::create($request->all());
 
-        return redirect ('/admin/users');
+        return redirect ('/admin/users');*/
 
-        //return $request->all();
+       //Third
+
+        $input = $request->all();
+
+        if($file = $request->file('photo_id')){
+
+            $name = time().$file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+        $input ['password'] = bcrypt($request->password);
+
+        User::create($input);
+
     }
 
     /**
